f6f9bde80edcb54eb7b611905be0b873a14620ec
Mazlai <micky31200@gmail.com>
2025-10-09 12:06:05 +0200

    Testing book

---
+++ Assets/Scripts/InteractWithBook.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using Oculus.Interaction;
+
+[RequireComponent(typeof(Grabbable))]
+public class InteractWithBook : MonoBehaviour
+{
+    [Header("Références")]
+    public GameObject closedBook;
+    public GameObject openedBook;
+    public GameObject textCanvas;
+
+    private Grabbable grabbable;
+    private bool isOpen = false;
+
+    void Awake()
+    {
+        grabbable = GetComponent<Grabbable>();
+        grabbable.WhenPointerEventRaised += HandlePointerEvent;
+    }
+
+    private void OnDestroy()
+    {
+        if (grabbable != null)
+            grabbable.WhenPointerEventRaised -= HandlePointerEvent;
+    }
+
+    private void HandlePointerEvent(PointerEvent evt)
+    {
+        switch (evt.Type)
+        {
+            case PointerEventType.Select:
+                OpenBook();
+                break;
+            case PointerEventType.Unselect:
+            case PointerEventType.Cancel:
+                CloseBook();
+                break;
+        }
+    }
+
+    void Start()
+    {
+        // États initiaux
+        if (closedBook) closedBook.SetActive(true);
+        if (openedBook) openedBook.SetActive(false);
+        if (textCanvas) textCanvas.SetActive(false);
+    }
+
+    void OpenBook()
+    {
+        if (isOpen) return;
+
+        isOpen = true;
+        if (closedBook) closedBook.SetActive(false);
+        if (openedBook) openedBook.SetActive(true);
+        if (textCanvas) textCanvas.SetActive(true);
+    }
+
+    void CloseBook()
+    {
+        if (!isOpen) return;
+
+        isOpen = false;
+        if (closedBook) closedBook.SetActive(true);
+        if (openedBook) openedBook.SetActive(false);
+        if (textCanvas) textCanvas.SetActive(false);
+    }
+}
